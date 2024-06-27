78e3c0840618b41a020091a82695e2b63985eb30
3b00d09 <bd315977@gmail.com>
2024-06-27 08:55:40 +0100

    feat: hero section and header of projects section, changed tailwind colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col bg-background min-h-[100dvh] text-text">
+      <header className="px-4 lg:px-6 h-14 flex items-center">
+        <p>icon</p>
+      </header>
+
+      <main className="flex-1 ">
+        <section className="w-full 2xl:max-w-[1536px] py-12 md:py-24 lg:py-32">
+          <div className="px-4 md:px-6 grid gap-8 lg:grid-cols-2 lg:gap-16">
+            <img
+              src="src/assets/personal-image.jpg"
+              alt="Personal Picture"
+              className="mx-auto aspect-[2/3] overflow-hidden rounded-xl object-contain object-center"
+              width={300}
+              height={450}
+            />
+            <div className="space-y-4">
+              <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
+                Abdullah Atiya
+              </h1>
+              <h2 className="md:text-xl font-light text-text-secondary">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo
+                atque, quaerat at corrupti repellat natus quam. Neque quasi
+                obcaecati error. Voluptate, fuga voluptatibus sit velit
+                necessitatibus ex odio magnam id.
+              </h2>
+            </div>
+          </div>
+        </section>
+
+        <section className="w-full py-12 md:py-24 lg:py-32 bg-secondary">
+          <div className="px-4 md:px-6 flex flex-col gap-4">
+            <h2 className="space-y-4 text-center text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
+              Projects
+            </h2>
+            <p className="text-center md:text-xl font-light">Check out some of my web development works.</p>
+          </div>
+        </section>
+        
+      </main>
+    </div>
+  );
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#f1f2f4',
+        'text-secondary' : "#a9b6d0",
+        'background': '#11151d',
+        'primary': '#d1e4fa',
+        'secondary': '#3a4b5f',
+        'accent': '#475e7b',
+      },
+    },
   },
   plugins: [],
 }
